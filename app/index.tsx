d5a5ebb6451852115fb9479d0292642063c8c725
suryav27 <160803358+suryav27@users.noreply.github.com>
2024-10-21 16:21:51 -0500

    map api integration and slight design fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,41 @@
-import { Text, View } from "react-native";
+// import { Text, View } from "react-native";
 
-export default function Index() {
+// export default function Index() {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>Dear SafeStep Team:</Text>
+//       <Text>We have a working app, for now...</Text>
+//       <Text>Woohoo!</Text>
+//     </View>
+//   );
+// }
+import React from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Dear SafeStep Team:</Text>
-      <Text>We have a working app, for now...</Text>
-      <Text>Woohoo!</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  map: {
+    width: '92%',
+    height: '55%',
+    borderRadius: 30,
+  },
+});
+
